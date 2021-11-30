1160e676fc2ad1d9acac02c7c75c055b068942cf
khaizinam <84978080+khaizinam@users.noreply.github.com>
2021-11-30 14:21:16 +0700

    as

--- temp.sql
+++ temp.sql
@@ -1,7 +1,28 @@
-﻿CREATE DATABASE E_LEARNING_TEACHING;  
+﻿/*********************************/
+/*                               */
+/*            CLEAR              */
+/*                               */
+/*********************************/
+DROP DATABASE E_LEARNING_TEACHING;  
+
+ALTER TABLE danh_sach_hoc_sinh_trong_lop DROP CONSTRAINT fk_danh_sach_id_sv;
+
+
+/*********************************/
+/*                               */
+/*         CREATE DB             */
+/*                               */
+/*********************************/
+
+CREATE DATABASE E_LEARNING_TEACHING;  
 go
 USE E_LEARNING_TEACHING; 
 go
+/*********************************/
+/*                               */
+/*            TABLE              */
+/*                               */
+/*********************************/
 CREATE TABLE Giang_Vien (
     ID VARCHAR(10) NOT NULL PRIMARY KEY,
     Ho_ten NVARCHAR(100) NOT NULL,
@@ -9,11 +30,7 @@ CREATE TABLE Giang_Vien (
     SDT VARCHAR(10),
     ID_Khoa VARCHAR(10) NOT NULL,
 );
-ALTER TABLE Giang_Vien ADD CONSTRAINT fk_Giang_Vien_id_khoa FOREIGN KEY (ID_Khoa) REFERENCES Khoa(ID);
-
-
 ---------------------------------------------------------------------------------------
-
 CREATE TABLE nhom_lop (
     ID VARCHAR(10) NOT NULL PRIMARY KEY,
 	ten VARCHAR(10) NOT NULL,
@@ -23,13 +40,7 @@ CREATE TABLE nhom_lop (
 	id_giang_vien VARCHAR(10) NOT NULL,
     id_monhoc VARCHAR(10) NOT NULL,
 );
-ALTER TABLE nhom_lop ADD CONSTRAINT fk_nhom_lop_id_monhoc FOREIGN KEY (id_monhoc) REFERENCES mon_hoc(ID);
-ALTER TABLE nhom_lop ADD CONSTRAINT fk_nhom_lop_id_giang_vien FOREIGN KEY (id_giang_vien) REFERENCES Giang_Vien(ID);
-ALTER TABLE nhom_lop ADD CONSTRAINT fk_nhom_lop_phong_hoc FOREIGN KEY (Phong_hoc) REFERENCES phong_hoc(name);
-
 ----------------------------------------------------------------------
-
-
 CREATE TABLE Sinh_Vien (
     ID VARCHAR(10) NOT NULL PRIMARY KEY,
     Ho_ten NVARCHAR(100) NOT NULL,
@@ -38,34 +49,27 @@ CREATE TABLE Sinh_Vien (
     TT_hoc NVARCHAR(100) NOT NULL,
     khoa_ID VARCHAR(10) NOT NULL,
 );
-ALTER TABLE Sinh_Vien ADD CONSTRAINT fk_Sinh_Vien_khoa_ID FOREIGN KEY (Khoa_ID) REFERENCES Khoa(ID);
-
 ----------------------------------------------------------------------
-
 CREATE TABLE phong_hoc (
     name VARCHAR(50) NOT NULL PRIMARY KEY,
 	toa NVARCHAR(100) NOT NULL,
 	co_so NVARCHAR(100) NOT NULL,
 );
-select * from phong_hoc;
-INSERT INTO phong_hoc VALUES ('202H3',N'H3',N'Co so 1');
 
 ----------------------------------------------------------------------
-
 CREATE TABLE khoa (
     ID VARCHAR(10) NOT NULL,
     name NVARCHAR(50) NOT NULL,
     PRIMARY KEY (ID)
 );
-INSERT INTO khoa values('100174',N'Khoa Học Máy Tinh'),('100175',N'Kĩ Thuật Máy Tính')
-DELETE FROM khoa;
+
 -----------------------------------------------------------------------------------
 CREATE TABLE mon_hoc (
     ID VARCHAR(10) NOT NULL PRIMARY KEY,
     Ten_MH NVARCHAR(100) NOT NULL,
     So_TC SMALLINT NOT NULL
 );
-INSERT INTO mon_hoc values('CO2004',N'Hệ Cơ sở DL','3'),('SP1008',N'Pháp Luat VN đại cương','4')
+
 -----------------------------------------------------------------------------------
 CREATE TABLE Giao_Trinh (
     ID VARCHAR(10) NOT NULL,
@@ -73,48 +77,29 @@ CREATE TABLE Giao_Trinh (
     Nam_xuatban VARCHAR(4) NOT NULL,
     PRIMARY KEY (ID)
 );
-
 CREATE TABLE Tac_Gia (
     ID VARCHAR(10) NOT NULL,
     Ho_Ten VARCHAR(50) NOT NULL,
     PRIMARY KEY (ID)
 );
-
-CREATE TABLE Quanly_Lop_Hoc (
-    ID_Nhomlop VARCHAR(9) NOT NULL,
-    ID_GV VARCHAR(10) NOT NULL,
-    ID_SV VARCHAR(10) NOT NULL,
-    PRIMARY KEY (ID_Nhomlop, ID_GV, ID_SV)
-);
-ALTER TABLE Quanly_Lop_Hoc ADD FOREIGN KEY (ID_Nhomlop) REFERENCES Nhom_Lop(ID);
-ALTER TABLE Quanly_Lop_Hoc ADD FOREIGN KEY (ID_GV) REFERENCES Giang_Vien(ID);
-ALTER TABLE Quanly_Lop_Hoc ADD FOREIGN KEY (ID_SV) REFERENCES Sinh_Vien(ID);
-
 ----------------------------------------------------------------------------
-
 CREATE TABLE danh_sach_hoc_sinh_trong_lop (
     ID_SV VARCHAR(10) NOT NULL,
 	ID_nhom_lop VARCHAR(10) NOT NULL ,
 );
-
+ALTER TABLE danh_sach_hoc_sinh_trong_lop
+DROP CONSTRAINT fk_danh_sach_id_sv; 
 CREATE TABLE giang_vien_quan_li_lop (
     ID_GV VARCHAR(10) NOT NULL,
 	ID_nhom_lop VARCHAR(10) NOT NULL ,
 );
-drop table danh_sach_hoc_sinh_trong_lop;
-select * from danh_sach_hoc_sinh_trong_lop;
-
-
--------------------------------------------------------------------------
+------------------------------------------------------------------------
 
 CREATE TABLE CT_Dao_Tao (
     Khoa_ID VARCHAR(10) NOT NULL,
     ID_Monhoc VARCHAR(6) NOT NULL,
     PRIMARY KEY (Khoa_ID, ID_Monhoc)
 );
-ALTER TABLE CT_Dao_Tao ADD FOREIGN KEY (Khoa_ID) REFERENCES Khoa(ID);
-ALTER TABLE CT_Dao_Tao ADD FOREIGN KEY (ID_Monhoc) REFERENCES Mon_Hoc(ID);
-
 ---------------------------------------------------------------------------
 
 CREATE TABLE Quanly_Mon_Hoc (
@@ -123,12 +108,6 @@ CREATE TABLE Quanly_Mon_Hoc (
     ID_GT VARCHAR(10) NOT NULL,
     PRIMARY KEY (ID_GV, ID_Monhoc, ID_GT)
 );
-
-ALTER TABLE Quanly_Mon_Hoc ADD FOREIGN KEY (ID_GV) REFERENCES Giang_Vien(ID);
-ALTER TABLE Quanly_Mon_Hoc ADD FOREIGN KEY (ID_Monhoc) REFERENCES Mon_Hoc(ID);
-ALTER TABLE Quanly_Mon_Hoc ADD FOREIGN KEY (ID_GT) REFERENCES Giao_Trinh(ID);
-
-
 ------------------------------------------------------------------------------
 
 CREATE TABLE Bien_Soan_GT (
@@ -137,17 +116,82 @@ CREATE TABLE Bien_Soan_GT (
     PRIMARY KEY (ID_GT, ID_TG)
 );
 
-
-
-ALTER TABLE Bien_Soan_GT ADD FOREIGN KEY (ID_GT) REFERENCES Giao_Trinh(ID);
-ALTER TABLE Bien_Soan_GT ADD FOREIGN KEY (ID_TG) REFERENCES Tac_Gia(ID);
+/*********************************/
+/*                               */
+/*         FOREIGN KEY           */
+/*                               */
+/*********************************/
+ALTER TABLE Giang_Vien ADD CONSTRAINT fk_Giang_Vien_id_khoa FOREIGN KEY (ID_Khoa) REFERENCES Khoa(ID);
+-----------------------------------------------------------------------------------------------------------
+ALTER TABLE nhom_lop ADD CONSTRAINT fk_nhom_lop_id_monhoc FOREIGN KEY (id_monhoc) REFERENCES mon_hoc(ID);
+ALTER TABLE nhom_lop ADD CONSTRAINT fk_nhom_lop_id_giang_vien FOREIGN KEY (id_giang_vien) REFERENCES Giang_Vien(ID);
+ALTER TABLE nhom_lop ADD CONSTRAINT fk_nhom_lop_phong_hoc FOREIGN KEY (Phong_hoc) REFERENCES phong_hoc(name);
+------------------------------------------------------------------------------------------------------------
+ALTER TABLE Sinh_Vien ADD CONSTRAINT fk_Sinh_Vien_khoa_ID FOREIGN KEY (Khoa_ID) REFERENCES Khoa(ID);
 --------------------------------------------------------------------
-DROP TABLE weak;
-CREATE TABLE weak (
-    ID_MH VARCHAR(6) NOT NULL,
-    weak VARCHAR(3) NOT NULL,
-    ID int IDENTITY(1,1) PRIMARY KEY
-);
-ALTER TABLE weak ADD FOREIGN KEY (ID_MH) REFERENCES Mon_hoc(ID);
-SELECT * FROM weak;
+ALTER TABLE danh_sach_hoc_sinh_trong_lop ADD CONSTRAINT fk_danh_sach_id_sv FOREIGN KEY (ID_SV) REFERENCES Sinh_vien(ID);
+ALTER TABLE danh_sach_hoc_sinh_trong_lop ADD CONSTRAINT fk_danh_sach_id_lop FOREIGN KEY (ID_nhom_lop) REFERENCES nhom_lop(ID);
 
+/*********************************/
+/*                               */
+/*         DATA                  */
+/*                               */
+/*********************************/
+INSERT INTO danh_sach_hoc_sinh_trong_lop
+VALUES 
+('1913779', '1101' )
+
+--------------------------------------------------------
+INSERT INTO phong_hoc 
+VALUES 
+('202H3', N'H3', N'Co so 1');
+
+----------------------------------------------------------
+INSERT INTO khoa 
+values
+('100174',N'Khoa Học Máy Tinh'),('100175',N'Kĩ Thuật Máy Tính')
+
+----------------------------------------------------------
+INSERT INTO mon_hoc 
+values
+('CO2004',N'Hệ Cơ sở DL','3'),('SP1008',N'Pháp Luat VN đại cương','4')
+
+----------------------------------------------------------
+INSERT INTO phong_hoc 
+VALUES 
+('202H3',N'H3',N'Co so 1');
+
+/*********************************/
+/*                               */
+/*            QUERY              */
+/*                               */
+/*********************************/
+SELECT * FROM Sinh_Vien;
+SELECT * FROM nhom_lop;
+
+-----------GOI danh sach GV-------------------
+SELECT Giang_Vien.Ho_ten,Giang_Vien.ID,Giang_Vien.Email,Giang_Vien.SDT ,khoa.name
+FROM Giang_Vien INNER JOIN khoa 
+ON Giang_Vien.ID_Khoa = khoa.ID
+WHERE Giang_Vien.ID = 10840;
+
+-----------GOI danh sach SV-------------------
+SELECT Sinh_Vien.Ho_ten as full_name, Sinh_Vien.ID as MSSV,Sinh_Vien.Email , Sinh_Vien.Lop,Sinh_Vien.TT_hoc as tinh_trang_hoc,khoa.name as khoa
+FROM Sinh_Vien INNER JOIN khoa 
+ON Sinh_Vien.khoa_ID = khoa.ID;
+
+-----------GOI danh sach nhom lop-------------------
+SELECT nhom_lop.ten as ten_nhom_lop,nhom_lop.tiet_bat_dau,nhom_lop.tiet_ket_thuc,nhom_lop.Phong_hoc,Giang_Vien.Ho_ten as giang_vien_phu_trach,mon_hoc.Ten_MH as mon_hoc
+FROM ((nhom_lop 
+INNER JOIN Giang_Vien ON nhom_lop.id_giang_vien = Giang_Vien.ID)
+INNER JOIN mon_hoc ON nhom_lop.id_monhoc = mon_hoc.ID);
+
+-----------GOI danh sach hoc sinh trong lop-------------------
+SELECT nhom_lop.ten as nhom_lop,Sinh_Vien.Ho_ten as full_name,Sinh_Vien.ID as MSSV , Sinh_Vien.Email,Sinh_Vien.Lop,khoa.name as khoa
+FROM (((danh_sach_hoc_sinh_trong_lop 
+INNER JOIN nhom_lop
+ON danh_sach_hoc_sinh_trong_lop.ID_nhom_lop = nhom_lop.ID)
+INNER JOIN Sinh_Vien
+ON danh_sach_hoc_sinh_trong_lop.ID_SV = Sinh_Vien.ID)
+INNER JOIN khoa
+ON Sinh_Vien.khoa_ID = khoa.ID);
