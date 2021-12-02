cc5f12c2bbf53297b91acb471ae130635708653c
khaizinam <84978080+khaizinam@users.noreply.github.com>
2021-12-02 13:00:45 +0700

    sql

---
+++ sqlserver/(1)ceate_data.sql
@@ -0,0 +1,178 @@
+﻿
+/*********************************/
+/*                               */
+/*         CREATE DB             */
+/*                               */
+/*********************************/
+
+
+CREATE DATABASE E_LEARNING_TEACHING_BETA_2;  
+go
+
+
+USE E_LEARNING_TEACHING_BETA_2; 
+go
+
+
+
+/*********************************/
+/*                               */
+/*            OBJECT             */
+/*                               */
+/*********************************/
+/* 1 */
+CREATE TABLE lecturer (
+    ID VARCHAR(10) NOT NULL PRIMARY KEY,
+    full_name NVARCHAR(100) NOT NULL,
+    Email NVARCHAR(100) NOT NULL,
+    p_number VARCHAR(10),
+    falcuty_ID VARCHAR(10) NOT NULL,
+);
+---------------------------------------------------------------------------------------
+/* 2 */
+CREATE TABLE class (
+    ID VARCHAR(10) NOT NULL PRIMARY KEY,
+	name VARCHAR(10) NOT NULL,
+    start_at VARCHAR(5) NOT NULL,
+	end_at VARCHAR(5) NOT NULL,
+    room VARCHAR(50) NOT NULL,
+	lecturer_ID VARCHAR(10) NOT NULL,
+    subject_ID VARCHAR(10) NOT NULL,
+);
+----------------------------------------------------------------------
+/* 3 */
+CREATE TABLE pupil (
+    ID VARCHAR(10) NOT NULL PRIMARY KEY,
+    full_name NVARCHAR(100) NOT NULL,
+    class VARCHAR(100) NOT NULL,
+    Email NVARCHAR(200) NOT NULL,
+    status NVARCHAR(100) NOT NULL,
+    falcuty_ID VARCHAR(10) NOT NULL,
+);
+----------------------------------------------------------------------
+/* 4 */
+CREATE TABLE author (
+    ID VARCHAR(10) NOT NULL,
+    full_name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (ID)
+);
+----------------------------------------------------------------------
+/* 5 */
+CREATE TABLE falcuty (
+    ID VARCHAR(10) NOT NULL,
+    name NVARCHAR(50) NOT NULL,
+    PRIMARY KEY (ID)
+);
+-----------------------------------------------------------------------------------
+/* 6 */
+CREATE TABLE subject (
+    ID VARCHAR(10) NOT NULL PRIMARY KEY,
+    name NVARCHAR(100) NOT NULL,
+    num_credit SMALLINT NOT NULL
+);
+-----------------------------------------------------------------------------------
+/* 7 */
+CREATE TABLE text_book (
+    ID VARCHAR(10) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    publishing_year VARCHAR(4) NOT NULL,
+    PRIMARY KEY (ID)
+);
+/*********************************/
+/*                               */
+/*        RELASHION SHIP         */
+/*                               */
+/*********************************/
+/* 8 */
+CREATE TABLE room (
+    name VARCHAR(50) NOT NULL PRIMARY KEY,
+	building NVARCHAR(100) NOT NULL,
+);
+----------------------------------------------------------------------------
+/* 9 */
+CREATE TABLE studentList (
+    pupil_ID VARCHAR(10) NOT NULL,
+	class_ID VARCHAR(10) NOT NULL ,
+);
+
+/* 10 */
+CREATE TABLE lecturerList (
+    lecturer_ID VARCHAR(10) NOT NULL,
+	class_ID VARCHAR(10) NOT NULL ,
+);
+
+------------------------------------------------------------------------
+/* 11 */
+CREATE TABLE education (
+    falcuty_ID VARCHAR(10) NOT NULL,
+    subject_ID VARCHAR(10) NOT NULL,
+);
+---------------------------------------------------------------------------
+/* 12 */
+CREATE TABLE subjectManage (
+    lecturer_ID VARCHAR(10) NOT NULL,
+    subject_ID VARCHAR(10) NOT NULL,
+    tex_book_ID VARCHAR(10) NOT NULL,
+);
+
+------------------------------------------------------------------------------
+/* 13 */
+CREATE TABLE compilation (
+    tex_book_ID VARCHAR(10) NOT NULL,
+    author_ID VARCHAR(10) NOT NULL,
+);
+/* 14 */
+CREATE TABLE registerSubject(
+    pupil_ID VARCHAR(10) NOT NULL,
+    subject_ID VARCHAR(10) NOT NULL,
+);
+/* 15 */
+CREATE TABLE creditPupil(
+    pupilID VARCHAR(10) NOT NULL,
+    numCredit SMALLINT NOT NULL,
+	totalCredit SMALLINT NOT NULL,
+);
+
+
+/*********************************/
+/*                               */
+/*         FOREIGN KEY           */
+/*                               */
+/*********************************/
+
+ALTER TABLE lecturer ADD CONSTRAINT fk_lecturer_falcutyID FOREIGN KEY (falcuty_ID) REFERENCES falcuty(ID);
+
+-----------------------------------------------------------------------------------------------------------
+ALTER TABLE class ADD CONSTRAINT fk_class_subjectID FOREIGN KEY (subject_ID) REFERENCES subject(ID);
+ALTER TABLE class ADD CONSTRAINT fk_class_lectureID FOREIGN KEY (lecturer_ID) REFERENCES lecturer(ID);
+ALTER TABLE class ADD CONSTRAINT fk_class_room FOREIGN KEY (room) REFERENCES room(name);
+
+------------------------------------------------------------------------------------------------------------
+ALTER TABLE pupil ADD CONSTRAINT fk_pupil_falcutyID FOREIGN KEY (falcuty_ID) REFERENCES falcuty(ID);
+
+--------------------------------------------------------------------
+ALTER TABLE studentList ADD CONSTRAINT fk_studentList_pupilID FOREIGN KEY (pupil_ID) REFERENCES pupil(ID);
+ALTER TABLE studentList ADD CONSTRAINT fk_studentList_classID FOREIGN KEY (class_ID) REFERENCES class(ID);
+
+--------------------------------------------------------------------
+ALTER TABLE lecturerList ADD CONSTRAINT fk_lecturerList_lectureID FOREIGN KEY (lecturer_ID) REFERENCES lecturer(ID);
+ALTER TABLE lecturerList ADD CONSTRAINT fk_lecturerList_classID FOREIGN KEY (class_ID) REFERENCES class(ID);
+
+--------------------------------------------------------------------
+ALTER TABLE education ADD CONSTRAINT fk_education_falcutyID FOREIGN KEY (falcuty_ID) REFERENCES falcuty(ID);
+ALTER TABLE education ADD CONSTRAINT fk_education_subjectID FOREIGN KEY (subject_ID) REFERENCES subject(ID);
+
+--------------------------------------------------------------------
+ALTER TABLE subjectManage ADD CONSTRAINT fk_subjectManage_lectureID FOREIGN KEY (lecturer_ID) REFERENCES lecturer(ID);
+ALTER TABLE subjectManage ADD CONSTRAINT fk_subjectManage_subjectID FOREIGN KEY (subject_ID) REFERENCES subject(ID);
+ALTER TABLE subjectManage ADD CONSTRAINT fk_subjectManage_texbookID FOREIGN KEY (tex_book_ID) REFERENCES text_book(ID);
+
+---------------------------------------------------------------------
+ALTER TABLE compilation ADD CONSTRAINT fk_compilation_texbookID FOREIGN KEY (tex_book_ID) REFERENCES text_book(ID);
+ALTER TABLE compilation ADD CONSTRAINT fk_compilation_authorID FOREIGN KEY (author_ID) REFERENCES author(ID);
+
+---------------------------------------------------------------------
+ALTER TABLE registerSubject ADD CONSTRAINT fk_registerSubject_pupilID FOREIGN KEY (pupil_ID) REFERENCES pupil(ID);
+ALTER TABLE registerSubject ADD CONSTRAINT fk_registerSubject_subjectID FOREIGN KEY (subject_ID) REFERENCES subject(ID);
+---------------------------------------------------------------------
+ALTER TABLE creditPupil ADD CONSTRAINT fk_creditPupil_pupilID FOREIGN KEY (pupilID) REFERENCES pupil(ID);
---
+++ sqlserver/(2)trigger.sql
@@ -0,0 +1,95 @@
+﻿
+/*********************************/
+/*                               */
+/*           Triger              */
+/*                               */
+/*********************************/
+
+
+create trigger createCredit on pupil AFTER INSERT
+AS
+begin
+	insert into creditPupil (pupilID,numCredit,totalCredit)
+	values ((SELECT inserted.ID FROM inserted),'','');
+end;
+go
+---------------------------------------------------------------
+
+create trigger deletePupil on pupil  
+INSTEAD OF DELETE 
+as
+begin
+	delete from creditPupil where pupilID = (SELECT deleted.ID FROM deleted);
+	delete from studentList  Where pupil_ID = (SELECT deleted.ID FROM deleted);
+	delete from registerSubject Where pupil_ID = (SELECT deleted.ID FROM deleted);
+	delete from pupil Where ID = (SELECT deleted.ID FROM deleted);
+end;
+go
+
+create trigger deleteLecturer on lecturer 
+INSTEAD OF DELETE
+as
+begin
+	delete from class where lecturer_ID = (SELECT deleted.ID FROM deleted);
+	delete from lecturerList  Where lecturer_ID = (SELECT deleted.ID FROM deleted);
+	delete from subjectManage Where lecturer_ID = (SELECT deleted.ID FROM deleted);
+	delete from lecturer Where ID = (SELECT deleted.ID FROM deleted);
+end;
+go
+
+create trigger deleteFalcuty on falcuty 
+INSTEAD OF DELETE
+as
+begin
+	delete from education where falcuty_ID = (SELECT deleted.ID FROM deleted);
+	delete from lecturer  Where falcuty_ID = (SELECT deleted.ID FROM deleted);
+	delete from pupil Where falcuty_ID = (SELECT deleted.ID FROM deleted);
+	delete from falcuty Where ID = (SELECT deleted.ID FROM deleted);
+end;
+go
+
+create trigger deleteSubject on subject 
+INSTEAD OF DELETE
+as
+begin
+	delete from education where subject_ID = (SELECT deleted.ID FROM deleted);
+	delete from subjectManage  Where subject_ID = (SELECT deleted.ID FROM deleted);
+	delete from registerSubject Where subject_ID = (SELECT deleted.ID FROM deleted);
+	delete from subject Where ID = (SELECT deleted.ID FROM deleted);
+end;
+go
+
+create trigger deleteTextbook on text_book 
+INSTEAD OF DELETE
+as
+begin
+	delete from subjectManage  Where tex_book_ID = (SELECT deleted.ID FROM deleted);
+	delete from compilation Where tex_book_ID = (SELECT deleted.ID FROM deleted);
+	delete from text_book Where ID = (SELECT deleted.ID FROM deleted);
+end;
+go
+create trigger deleteClass on class 
+INSTEAD OF DELETE
+as
+begin
+	delete from studentList  Where class_ID = (SELECT deleted.ID FROM deleted);
+	delete from lecturerList Where class_ID = (SELECT deleted.ID FROM deleted);
+	delete from class Where ID = (SELECT deleted.ID FROM deleted);
+end;
+go
+create trigger deleteAuthor on author 
+INSTEAD OF DELETE
+as
+begin
+	delete from compilation  Where author_ID = (SELECT deleted.ID FROM deleted);
+	delete from author Where ID = (SELECT deleted.ID FROM deleted);
+end;
+go
+------------------------------------------------------------------------------
+
+
+
+
+
+
+
